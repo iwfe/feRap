ea3282470c13ceb2a0586c8da9b464814c8f5024
lizongqing <lizongqing.sh@superjia.com>
2017-07-03 19:30:46 +0800

    modify header style

--- static/src/components/FeHeader.vue
+++ static/src/components/FeHeader.vue
@@ -1,19 +1,19 @@
 <template>
   <header id="header">
-  <div class="header-wrap clearfix">
-    <div class="menu">
-      <a v-for="item in menus" class="item" :href="item.url"> {{item.text}} </a>
-      <div class="right">
-        <a class="item msg-span" href="/message">消息 (<span class="msg-count">3</span>)</a>
-        <div class="item" tabindex="0">
-          <span>lancui <i class="dropdown icon"></i></span>
-          <div tabindex="-1" class="drop-items">
-            <a class="item" href="/user/profile">个人设置</a>
-            <a class="item" href="/logout">退出</a> </div>
+    <div class="header-wrap clearfix">
+      <div class="menu">
+        <a v-for="(item, index) in menus" class="item" :class="{active: active == index}" v-bind:key="index" :href="item.url"> {{item.text}} </a>
+        <div class="right">
+          <a class="item msg-span" href="/message">消息 (<span class="msg-count">3</span>)</a>
+          <div class="item" tabindex="0">
+            <span>lancui <i class="dropdown icon"></i></span>
+            <div tabindex="-1" class="drop-items">
+              <a class="item" href="/user/profile">个人设置</a>
+              <a class="item" href="/logout">退出</a> </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 </header>
 </template>
 
@@ -27,18 +27,38 @@ export default {
         {url: '/teams/index?prdId=m3tnaO', text: '团队'},
         {url: '/api/index', text: 'API'},
         {url: '/calendar', text: '日历'}
-      ]
+      ],
+      active: 0
+    }
+  },
+  beforeMount () {
+    this.urlAnalyze()
+  },
+  methods: {
+    urlAnalyze () {
+      const pathname = location.pathname
+      const pathArr = ['/index', '/teams', '/api', '/calendar']
+      for (let i = 0; i < pathArr.length; i++) {
+        const exp = new RegExp('^' + pathArr[i] + '\\b')
+        // console.log(pathname, exp)
+        if (exp.test(pathname)) {
+          this.active = i
+        }
+      }
+      console.log(this.active)
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="less">
 #header {
   background-color: #fff;
+  height: 64px;
   border-bottom: 1px solid #e9e9e9;
   box-shadow: 0 1px 6px rgba(99,99,99,.2);
+  font-size: 14px;
 }
 .header-wrap {
   width: 100%;
@@ -52,16 +72,30 @@ export default {
   font-weight: 400;
 }
 .item {
+  box-sizing: border-box;
+  min-width: 72px;
   border-bottom-color: transparent;
   border-bottom-style: solid;
   border-radius: 0;
-  item-align: end;
-  padding: 10px 20px;
+  height: 64px;
+  line-height: 64px;
+  align-items: end;
+  padding-left: 20px;
+  padding-right: 20px;
   align-self: flex-end;
   margin: 0 0 -2px;
   border-bottom-width: 2px;
-  -webkit-transition: color .1s ease;
-  transition: color .1s ease;
+  -webkit-transition: color,border-bottom-color .2s ease;
+  transition: color,border-bottom-color .2s ease;
+  &:hover{
+    border-bottom-color: #20a0ff;
+    color: #20a0ff;
+  }
+  &.active{
+    border-bottom-color: #20a0ff;
+    color: #20a0ff;
+    font-weight: 700;
+  }
 }
 .right {
   display: flex;
